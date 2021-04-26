86e4d029fc175af86c21881715f418c266fda4e0
Hannah M <hannahemalm616@gmail.com>
2021-04-26 21:39:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 'X'
+      WHERE grade = ?
     SQL
     DB[:conn].execute(sql, x).map do |row|
     self.new_from_db(row)
